1e37eb71e25f1be5ec0b9e2e195f10d4e4b24b0c
emlil <46994670+emlil@users.noreply.github.com>
2019-02-04 15:41:21 +0100

    Satt opp div funksjoner

--- core/src/com/mygdx/frick/Tile.java
+++ core/src/com/mygdx/frick/Tile.java
@@ -1,17 +1,33 @@
 package com.mygdx.frick;
 
 
-public class Tile {
-     String type;
-     boolean occupied;
+import java.awt.image.TileObserver;
+import java.util.List;
+
+public class Tile  {
+    List<TileObject> content;
+    // boolean occupied;
 
 
     public Tile() {
 
     }
 
-    public Tile(String type) {
-        this.type=type;
-        occupied=false;
+    public Tile( TileObject object) {
+
+        content.add(object);
     }
+
+    public void AddObject(TileObject object){
+        content.add(object);
+    }
+    public Boolean Contains(TileObject object){
+        return content.contains(object);
+
+    }
+
+    public List<TileObject> getContent() {
+        return content;
+    }
+
 }
