34f9d69f44660208a2c5b7508838b234be5f5dcd
Andrey <beliy255@gmail.com>
2019-02-12 15:09:04 +0100

    1. libgdx image basics

--- src/main/java/inf112/skeleton/app/ImageTest.java
+++ src/main/java/inf112/skeleton/app/ImageTest.java
@@ -4,16 +4,47 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class ImageTest implements ApplicationListener {
-    SpriteBatch batch;
-    Texture img;
+    private Stage stage;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("assets/image.jpg");
+        stage = new Stage(new ScreenViewport());
+        Texture texture = new Texture("assets/image.jpg");
+
+        Image image1 = new Image(texture);
+        image1.setSize(texture.getWidth()/1.5f, texture.getHeight()/1.5f);
+        image1.setPosition(Gdx.graphics.getWidth()/3f - image1.getWidth()/2f,
+                Gdx.graphics.getHeight()*2f/3f - image1.getHeight()/2f);
+        stage.addActor(image1);
+
+        Image image2 = new Image(texture);
+        image2.setSize(texture.getWidth()/1.5f, texture.getHeight()/1.5f);
+        image2.setPosition(Gdx.graphics.getWidth()*2f/3f - image2.getWidth()/2f,
+                Gdx.graphics.getHeight()*2f/3f - image2.getHeight()/2f);
+        image2.setOrigin(image2.getWidth()/2f, image2.getHeight()/2f);
+        image2.rotateBy(45);
+        stage.addActor(image2);
+
+        Image image3 = new Image(texture);
+        image3.setSize(texture.getWidth()/2f, texture.getHeight()/2f);
+        image3.setPosition(Gdx.graphics.getWidth()/3f - image3.getWidth()/2f,
+                Gdx.graphics.getHeight()/3f - image3.getHeight()/2f);
+        stage.addActor(image3);
+
+        texture.setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
+        TextureRegion textureRegion = new TextureRegion(texture);
+        textureRegion.setRegion(2, 2, texture.getWidth()*2f, texture.getHeight()*2f);
+        Image image4 = new Image(textureRegion);
+        image4.setSize(300, 200);
+        image4.setPosition(Gdx.graphics.getWidth()*2f/3f - image4.getWidth()/2f,
+                Gdx.graphics.getHeight()/3f - image4.getHeight()/2f);
+        stage.addActor(image4);
     }
 
     @Override
@@ -23,11 +54,10 @@ public class ImageTest implements ApplicationListener {
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        stage.act();
+        stage.draw();
     }
 
     @Override
@@ -42,7 +72,6 @@ public class ImageTest implements ApplicationListener {
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
+
     }
 }
--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -1,18 +1,16 @@
 package inf112.skeleton.app;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "hello-world";
-        cfg.width = 480;
-        cfg.height = 320;
+        cfg.title = "Scene2d Image Manipulation";
+        cfg.width = 1680;
+        cfg.height = 1050;
 
         // new LwjglApplication(new HelloWorld(), cfg);
-        new LwjglApplication(new ImageTest());
+        new LwjglApplication(new ImageTest(), cfg);
     }
 }
