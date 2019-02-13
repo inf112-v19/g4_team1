c6b884d9456596b10767a5d13dd19fae67529e1d
Andrey <beliy255@gmail.com>
2019-02-13 21:24:56 +0100

    implemented a simple Drop game from the dev tutorial

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -2,11 +2,12 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import inf112.skeleton.app.gameclasses.Drop;
 import inf112.skeleton.app.gameclasses.TestTiledMap;
 
 public class Main {
     public static void main(String[] args) {
-        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        /*LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "Scene2d Image Manipulation";
         cfg.useGL30 = true;
         cfg.width = 1680;
@@ -15,6 +16,13 @@ public class Main {
         // new LwjglApplication(new HelloWorld(), cfg);
         // new LwjglApplication(new ImageTest(), cfg);
         // new LwjglApplication(new LabelAndFont(), cfg);
-        new LwjglApplication(new TestTiledMap(), cfg);
+        new LwjglApplication(new TestTiledMap(), cfg);*/
+
+        LwjglApplicationConfiguration cfg2 = new LwjglApplicationConfiguration();
+
+        cfg2.title = "Drop";
+        cfg2.width = 800;
+        cfg2.height = 480;
+        new LwjglApplication(new Drop(), cfg2);
     }
 }
