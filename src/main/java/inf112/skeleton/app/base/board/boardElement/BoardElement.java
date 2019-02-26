18fe6e68b805ad1548bd2dc91b0cb75da09592c1
TobiasE <tseilertsen@gmail.com>
2019-02-26 19:02:18 +0100

    changed board elements to use pos and all now extends BoardElement

--- src/main/java/inf112/skeleton/app/base/board/boardElement/BoardElement.java
+++ src/main/java/inf112/skeleton/app/base/board/boardElement/BoardElement.java
@@ -1,22 +1,24 @@
 package inf112.skeleton.app.base.board.boardElement;
 
 import inf112.skeleton.app.base.board.Board;
+import inf112.skeleton.app.base.board.ITileObject;
 import inf112.skeleton.app.base.utils.Pos;
 
-public abstract class ActiveElement implements  IActiveElement {
+public abstract class BoardElement implements ITileObject {
+    protected final Board board;
     protected Pos pos;
     protected char symbol;
     protected String name;
-    protected Board board;
 
-    public ActiveElement(Pos pos, char symbol, String name){
+    public BoardElement(Pos pos, char symbol,  Board board){
 
         this.pos = pos;
         this.symbol = symbol;
         this.name = name;
+        this.board = board;
     }
 
-   public Pos getPos(){
+    public Pos getPos(){
        return pos;
    }
 
--- src/main/java/inf112/skeleton/app/base/board/boardElement/Wall.java
+++ src/main/java/inf112/skeleton/app/base/board/boardElement/Wall.java
@@ -3,61 +3,18 @@ package inf112.skeleton.app.base.board.boardElement;
 import inf112.skeleton.app.base.board.Board;
 import inf112.skeleton.app.base.board.ITileObject;
 import inf112.skeleton.app.base.utils.Direction;
+import inf112.skeleton.app.base.utils.Pos;
 
-public class Wall implements ITileObject {
+public class Wall extends BoardElement implements ITileObject {
 
     private Direction wallDir;
-    private int x;
-    private int y;
-    private char symbol;
-    private String name;
-    private Board board;
 
-    public Wall(Direction dir, int x, int y, char symbol, Board board) {
+    public Wall(Direction dir, Pos pos, char symbol, Board board) {
+        super(pos, symbol, board);
         this.wallDir = dir;
-        this.x = x;
-        this.y = y;
-        this.symbol = symbol;
-        this.board = board;
-        name="Wall";
     }
 
-    public Direction getWallDir(){
+    public Direction getWallDir() {
         return wallDir;
     }
-    @Override
-    public int getX() {
-        return x;
-    }
-
-    @Override
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public Integer[] getXY() {
-        return new Integer[2];
-    }
-
-    @Override
-    public char getSymbol() {
-        return symbol;
-    }
-
-    @Override
-    public void setSymbol(char symbol) {
-        this.symbol=symbol;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name=name;
-    }
-
 }
