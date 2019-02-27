4bdcb273131bd709f5c18a7fc635c83b243eb11c
ingri e. Brendsdal <ibrendsdal@gmail.com>
2019-02-28 00:18:57 +0100

    created test class for Pushermethod

--- src/main/java/inf112/skeleton/app/base/board/boardElement/Pusher.java
+++ src/main/java/inf112/skeleton/app/base/board/boardElement/Pusher.java
@@ -4,7 +4,7 @@ import inf112.skeleton.app.base.board.Board;
 import inf112.skeleton.app.base.utils.Direction;
 import inf112.skeleton.app.base.utils.Pos;
 
-public class Pusher extends BoardElement {
+public class Pusher extends BoardElement implements IActiveElement{
 
     private Direction pushDir;
     private Board board;
---
+++ src/test/java/inf112/skeleton/app/boardelementtests/PusherTest.java
@@ -0,0 +1,19 @@
+package inf112.skeleton.app.boardelementtests;
+import inf112.skeleton.app.base.actors.*;
+import inf112.skeleton.app.base.board.Board;
+import inf112.skeleton.app.base.board.Tile;
+import inf112.skeleton.app.base.board.boardElement.Pusher;
+import inf112.skeleton.app.base.utils.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PusherTest {
+
+
+    @Test
+    void pushTurn() {
+
+    }
+
+}
