26aff0049a6f7fa693395636ae11b81543bc0da9
ingri e. Brendsdal <ibrendsdal@gmail.com>
2019-03-13 15:18:36 +0100

    Working on the test method for TurnConveyor

--- src/test/java/inf112/skeleton/app/boardelementtests/TurnConveyorTest.java
+++ src/test/java/inf112/skeleton/app/boardelementtests/TurnConveyorTest.java
@@ -1,8 +1,42 @@
 package inf112.skeleton.app.boardelementtests;
 
 import inf112.skeleton.app.base.actors.IRobot;
+import inf112.skeleton.app.base.actors.Player;
+import inf112.skeleton.app.base.actors.Robot;
+import inf112.skeleton.app.base.board.Board;
+import inf112.skeleton.app.base.board.Tile;
+import inf112.skeleton.app.base.board.boardelement.Pusher;
+import inf112.skeleton.app.base.utils.Direction;
+import inf112.skeleton.app.base.utils.Pos;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TurnConveyorTest {
+
+    @Test
+    void robotMove() {
+        Board board = new Board(10, 10);
+        Pos pos = new Pos(0, 0);
+
+
+        Tile tile = new Tile();
+        Pusher pusher = new Pusher(Direction.NORTH, pos, 'a', board);
+        board.addTileObject(pusher);
+
+
+        Robot robot = new Robot(pos, Direction.EAST, new Player("player"), board);
+        board.addTileObject(robot);
+
+        assertEquals(robot.getPos(), pusher.getPos());
+        Pos newPos = new Pos(0, 1);
+
+        pusher.activate();
+
+        //should be positon after pushing EAST.
+        assertEquals(robot.getPos(), newPos);
+
+    }
 
 
 }
