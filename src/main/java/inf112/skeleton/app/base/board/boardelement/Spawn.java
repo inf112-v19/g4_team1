cfd39ed81aa04def1a7d894372d120ddc720abf8
sca003 <simen.carstensen@student.uib.no>
2019-03-17 16:42:59 +0100

    Created Spawn-class in boardelements

---
+++ src/main/java/inf112/skeleton/app/base/board/boardelement/Spawn.java
@@ -0,0 +1,11 @@
+package inf112.skeleton.app.base.board.boardelement;
+
+import inf112.skeleton.app.base.board.Board;
+import inf112.skeleton.app.base.utils.Pos;
+
+public class Spawn extends BoardElement {
+
+    public Spawn(Pos pos, char symbol, Board board) {
+        super(pos, symbol, board);
+    }
+}
