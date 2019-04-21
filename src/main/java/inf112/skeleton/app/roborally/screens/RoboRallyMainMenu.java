c814e15692cab73e3509de1492610ddf4c5c0320
KassaPng <simencarstensen@gmail.com>
2019-04-21 12:35:42 +0200

    shortcut for starting game

--- src/main/java/inf112/skeleton/app/roborally/screens/RoboRallyMainMenu.java
+++ src/main/java/inf112/skeleton/app/roborally/screens/RoboRallyMainMenu.java
@@ -72,11 +72,11 @@ public class RoboRallyMainMenu implements Screen {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 // go to main game screen
-                roboRally.setScreen(new PreferencesScreen(roboRally));
+                //roboRally.setScreen(new PreferencesScreen(roboRally));
                 ArrayList<String> names = new ArrayList<>();
                 names.add("player1");
                 names.add("Player2");
-               // roboRally.setScreen(new RoboRallyGame(roboRally, names));
+                roboRally.setScreen(new RoboRallyGame(roboRally, names));
                 dispose();
                 return true;
             }
