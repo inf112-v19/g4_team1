52d5600b7ed40cf097576f9333f614f87ea93a84
emlil <46994670+emlil@users.noreply.github.com>
2019-05-02 11:15:49 +0200

    setup for endgame
    Co-Authored-By: ibr035 <ibr035@users.noreply.github.com>

--- src/main/java/inf112/skeleton/app/roborally/screens/endGame.java
+++ src/main/java/inf112/skeleton/app/roborally/screens/endGame.java
@@ -1,155 +1,130 @@
 package inf112.skeleton.app.roborally.screens;
 
-
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
+
+import com.badlogic.gdx.Input.TextInputListener;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import inf112.skeleton.app.roborally.RoboRally;
-import inf112.skeleton.app.roborally.screens.RoboRallyGame;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class endGame implements Screen, InputProcessor, ActionListener {
-    private RoboRally roboRally;
-    private OrthographicCamera camera;
-    private TiledMap map;
-    private OrthogonalTiledMapRenderer mapRenderer;
+public class endGame implements Screen {
+
     private Stage stage;
-    private Skin uiskin;
-    private SpriteBatch batch;
-    private BitmapFont font;
-    private String winner;
+    private RoboRally roboRally;
+    private Table playerTable;
+    private ArrayList<String> names = new ArrayList<>();
+    private Skin skin;
+    private ArrayList<String> maps= new ArrayList<>();
+    private int mapindex = 0;
+    Image mapimg;
 
-    public endGame(RoboRally roboRally, String name) {
+
+
+
+    public endGame(RoboRally roboRally) {
         this.roboRally = roboRally;
-        camera = new OrthographicCamera();
-        winner = " Game ended with no winner";
-        font = new BitmapFont();
-        font.setColor(Color.RED);
-        font.getData().scale(13);
-        batch = new SpriteBatch();
 
-        FitViewport viewport = new FitViewport(1279, 639, camera);
-        stage = new Stage(viewport, roboRally.batch);
+        stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
+        this.maps.add("assets/roborally/game_board1.tmx");
+        this.maps.add("assets/roborally/game_board2.tmx");
+        this.maps.add("assets/roborally/game_board3.tmx");
 
-        //buttons
-        uiskin = new Skin(Gdx.files.internal("assets/roborally/skin/comic-ui.json"));
-
-        Button newGame = new TextButton("new Game", uiskin);
-        newGame.setSize(200, 111);
-        newGame.setPosition(300, 100);
-        newGame.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                roboRally.setScreen(new PreferencesScreen(roboRally));
-                //ArrayList<String> names = new ArrayList<>();
-                //names.add("player1");
-                //names.add("Player2");
-                //roboRally.setScreen(new RoboRallyGame(roboRally, names));
-                dispose();
-                return true;
-
-            }
-        });
-        stage.addActor(newGame);
-        Button exit = new TextButton("exit", uiskin);
-        exit.setSize(200, 111);
-        exit.setPosition(700, 100);
-        exit.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                Gdx.app.exit();
-                return true;
-            }
-        });
-        stage.addActor(exit);
-
-
-    }
-
-    @Override
-    public boolean keyDown(int i) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int i) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char c) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int i, int i1, int i2, int i3) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int i, int i1, int i2, int i3) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int i, int i1, int i2) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int i, int i1) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int i) {
-        return false;
     }
 
     @Override
     public void show() {
+        Texture background = new Texture("assets/roborally/Robot-Wall.jpg");
+        Image img = new Image(background);
+        img.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage.addActor(img);
 
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(false);
+        stage.addActor(table);
+        skin = new Skin(Gdx.files.internal("assets/roborally/skin/comic-ui.json"));
+
+        TextButton mainMenu = new TextButton("Main menu", skin);
+        TextButton exit = new TextButton("Exit", skin);
+
+        table.add(mainMenu).fillX().uniformX().pad(10);
+        table.row();
+        table.add(exit).fillX().uniformX().pad(10);
+        table.row();
+
+
+
+        //changeImage();
+        exit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+        mainMenu.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                roboRally.setScreen(new PreferencesScreen(roboRally));
+                dispose();
+            }
+        });
+    }
+
+
+    private void changeImage(){
+        mapimg.setDrawable(new SpriteDrawable(new Sprite(new Texture("assets/roborally/mapimages/map"+(mapindex+1)+".png"))));
+
+
+        /*
+        mapimg = new Image(new Texture("assets/roborally/mapimages/map"+(mapindex+1)+".png"));
+        mapimg.setSize(400, 400);
+        mapimg.setPosition(500, 500);
+        stage.addActor(mapimg);
+        */
+
+
+    }
+    private void errorMsg(String err){
+        Dialog dialog = new Dialog("warning", skin){
+            public void result(Object obj){
+            }
+        };
+        dialog.setSize(300, 300);
+
+        dialog.button("OK", true);
+        dialog.show(stage);
+
+        dialog.text(err).setSize(300, 300);
+        dialog.getTitleLabel().setFontScale(1);
     }
 
     @Override
     public void render(float v) {
-        //  Gdx.gl.glClearColor(119/255f,136/255f,153/255f,1);
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        roboRally.batch.setProjectionMatrix(camera.combined);
-        //mapRenderer.setView(camera);
-        // mapRenderer.render();
-        stage.act();
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
-        batch.begin();
-        font.draw(batch, winner, 500, 1200);
-        batch.end();
-
 
     }
 
     @Override
-    public void resize(int i, int i1) {
-
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -169,12 +144,9 @@ public class endGame implements Screen, InputProcessor, ActionListener {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        skin.dispose();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
-    }
 }
-
