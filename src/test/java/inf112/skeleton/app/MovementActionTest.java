6e92cbf15ccf19c2260aaf7815fbf1eab0b3e415
benq66 <beliy255@gmail.com>
2019-05-03 13:48:10 +0200

    wip on the report 2

--- src/test/java/inf112/skeleton/app/MovementActionTest.java
+++ src/test/java/inf112/skeleton/app/MovementActionTest.java
@@ -1,31 +1,14 @@
 package inf112.skeleton.app;
 
-import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
-import inf112.skeleton.app.base.actors.IRobot;
-import inf112.skeleton.app.base.actors.Player;
-import inf112.skeleton.app.base.actors.Robot;
-import inf112.skeleton.app.base.board.Board;
-import inf112.skeleton.app.base.board.IBoard;
-import inf112.skeleton.app.base.utils.Direction;
-import inf112.skeleton.app.base.utils.Pos;
-import inf112.skeleton.app.roborally.RoboRally;
-import inf112.skeleton.app.roborally.screens.RoboRallyGame;
 import inf112.skeleton.app.roborally.screens.graphics.MovementAction;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MovementActionTest {
 
     @Test
     void timeTest(){
-
         MovementAction normal = MovementAction.NORMAL;
         MovementAction fast = MovementAction.FAST;
-        assert(normal.getActionTime() <= fast.getActionTime());
-
-
+        assert(normal.getActionTime() >= fast.getActionTime());
     }
 }
