a37eeb7d8825d27e016adb885886bdf18df0c5f2
TobiasE <tseilertsen@gmail.com>
2019-05-03 13:34:27 +0200

    fixed test

--- src/test/java/inf112/skeleton/app/MovementActionTest.java
+++ src/test/java/inf112/skeleton/app/MovementActionTest.java
@@ -24,8 +24,6 @@ public class MovementActionTest {
 
         MovementAction normal = MovementAction.NORMAL;
         MovementAction fast = MovementAction.FAST;
-        assert(normal.getActionTime() <= fast.getActionTime());
-
-
+        assert(normal.getActionTime() >= fast.getActionTime());
     }
 }
