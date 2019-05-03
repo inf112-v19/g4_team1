b49b0eb1beb654d09423866adcf571fb567bc143
ingri e. Brendsdal <ibrendsdal@gmail.com>
2019-05-03 12:45:46 +0200

    changed background color in endGame

--- src/main/java/inf112/skeleton/app/roborally/screens/EndGame.java
+++ src/main/java/inf112/skeleton/app/roborally/screens/EndGame.java
@@ -74,7 +74,6 @@ public class EndGame implements Screen {
 
     @Override
     public void render(float v) {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
